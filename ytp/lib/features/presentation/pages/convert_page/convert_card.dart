26b9c9b2464a702005931812a9bea18fec9bec7e
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2024-08-18 01:42:49 +0600

    done

--- ytp/lib/features/presentation/pages/convert_page/convert_card.dart
+++ ytp/lib/features/presentation/pages/convert_page/convert_card.dart
@@ -18,6 +18,19 @@ class ConvertCard extends StatefulWidget {
 
 class _ConvertCardState extends State<ConvertCard> {
   int? selectedContainerIndex;
+  late TextEditingController currencyController;
+  @override
+  void initState() {
+    super.initState();
+    currencyController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    currencyController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultContainer(
@@ -31,6 +44,7 @@ class _ConvertCardState extends State<ConvertCard> {
             style: widget.theme.textTheme.titleSmall,
             hintText: 'Currency',
             hintStyle: widget.theme.textTheme.displayLarge,
+            controller: currencyController,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -40,6 +54,7 @@ class _ConvertCardState extends State<ConvertCard> {
                 onTap: () {
                   setState(() {
                     selectedContainerIndex = index;
+                    currencyController.text = widget.moneys[index];
                   });
                 },
                 child: DefaultContainer(
@@ -51,7 +66,7 @@ class _ConvertCardState extends State<ConvertCard> {
                       : widget.theme.colorScheme.surface,
                   child: Text(widget.moneys[index],
                       style: selectedContainerIndex == index
-                          ? widget.theme.textTheme.displayLarge
+                          ? widget.theme.textTheme.bodySmall
                           : widget.theme.textTheme.displayMedium),
                 ),
               ),
@@ -62,6 +77,7 @@ class _ConvertCardState extends State<ConvertCard> {
             style: widget.theme.textTheme.titleSmall,
             hintText: 'Amount',
             hintStyle: widget.theme.textTheme.displayLarge,
+            keyboardType: TextInputType.number,
           ),
         ],
       ),
